9b66febb6630abeabdcf8a52d9de1d9675395c61
Neil Ding <bbbear444@gmail.com>
2015-02-18 01:58:38 -0500

    simple web UI

--- routes/index.js
+++ routes/index.js
@@ -33,8 +33,14 @@ router.get('/', function(req, res, next) {
       }
       return false;
     });
-
-    res.send(ret);
+    if (req.xhr) {
+      res.send(ret);
+    } else {
+      res.render('index', {
+        title: 'Game Highlights',
+        videos: ret
+      });
+    }
   });
 });
 
