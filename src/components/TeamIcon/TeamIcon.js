f1a035fe7a626f50eebf4cc086bcf5ef194fea86
GingerBear <bbbear444@gmail.com>
2016-12-02 18:50:45 -0500

    adjust the rank style

--- src/components/TeamIcon/TeamIcon.js
+++ src/components/TeamIcon/TeamIcon.js
@@ -3,15 +3,20 @@ import { getState } from '../../lib/global-state';
 import './TeamIcon.css';
 
 class TeamIcon extends Component {
+  renderRank(rk) {
+    var rkNum = rk.replace('e', '').replace('w', '');
+    return <span className={`conf-${rk[0]}`}>{rkNum}</span>;
+  }
   render() {
     var rankings = getState().rankings;
     return (
       <div className='TeamIcon'>
+        {this.renderRank(rankings[this.props.teamInfo.triCode])}
         <img src={`images/${this.props.teamInfo.triCode.toLowerCase()}.gif`} alt={this.props.teamInfo.triCode} />
         <span>
           {this.props.teamInfo.triCode}
           <span className="WinLoss">
-            ({this.props.teamInfo.win}-{this.props.teamInfo.loss}) {rankings[this.props.teamInfo.triCode]}
+            ({this.props.teamInfo.win}-{this.props.teamInfo.loss})
           </span>
         </span>
       </div>
