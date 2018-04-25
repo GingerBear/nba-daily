e2330a7dcde9e8d367ff03b7455dd501c6b832b2
GingerBear <bbbear444@gmail.com>
2018-04-24 23:40:00 -0400

    use yarn

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-npm install
-npm run build
+yarn
+yarn run build
 git clone https://GingerBear:34d545d96b273a545f23e0389fc60d84c6b1e83b@github.com/GingerBear/gingerbear.github.io.git
 rm -rf gingerbear.github.io/nba
 cp -R build gingerbear.github.io/nba
