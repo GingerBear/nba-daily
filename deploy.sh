a4eb8929e0a76bbbdc937067f8710c0e2893b131
GingerBear <bbbear444@gmail.com>
2018-04-24 23:59:37 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 yarn
 yarn run build
-git clone https://GingerBear:34d545d96b273a545f23e0389fc60d84c6b1e83b@github.com/GingerBear/gingerbear.github.io.git
+git clone https://GingerBear:$GITHUB_TOKEN@github.com/GingerBear/gingerbear.github.io.git
 rm -rf gingerbear.github.io/nba
 cp -R build gingerbear.github.io/nba
 cd gingerbear.github.io
-git add .
+git add . -A
 git commit -am "update nba-daily"
 git push origin master
 cd ..
